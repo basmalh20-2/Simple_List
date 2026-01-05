6595dd254df92818340af2fe4a8327ecf8698572
Basmalh Mokhtar <bsmltmkhtar404@gmail.com>
2026-01-05 20:42:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = 'todo-cache-v1';
+const filesToCache = [
+    'index.html',
+    'style.css',
+    'javas.js',
+    'small.png',
+    'large.png',
+    'empty massage.JPG'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+        return response || fetch(event.request).catch(() => {
+            // لو أوفلاين والملف مش موجود في cache، ارجع الصفحة الرئيسية
+            return caches.match('index.html');
+        });
+        })
+    );
+});
+
